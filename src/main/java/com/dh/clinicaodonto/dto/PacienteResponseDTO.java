e8950255c9f32e875869c89f3f6910474b423fe7
AlanMNZS <alan.mnzs@gmail.com>
2022-11-19 00:57:50 -0300

    Classe DTO Paciente

---
+++ src/main/java/com/dh/clinicaodonto/dto/PacienteResponseDTO.java
@@ -0,0 +1,34 @@
+package com.dh.clinicaodonto.dto;
+
+
+import com.dh.clinicaodonto.domain.Endereco;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class PacienteResponseDTO {
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private String sobrenome;
+    @NotBlank
+    private String matricula;
+    @NotBlank
+    private PasswordDTO usuario;
+
+    private Endereco endereco;
+    private Date dataDeCadastro;
+
+
+}
