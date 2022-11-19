17b295a7697a8f2201e94cb0d3184794bc6c1277
AlanMNZS <alan.mnzs@gmail.com>
2022-11-18 23:33:24 -0300

    Classe DTO Consulta

---
+++ src/main/java/com/dh/clinicaodonto/dto/ConsultaDTO.java
@@ -0,0 +1,25 @@
+package com.dh.clinicaodonto.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ConsultaDTO {
+    @NotBlank
+    private PacienteResponseDTO paciente;
+    @NotBlank
+    private DentistaResponseDTO dentista;
+    @NotBlank
+    private Timestamp dataHoraAgndamento;
+
+}
