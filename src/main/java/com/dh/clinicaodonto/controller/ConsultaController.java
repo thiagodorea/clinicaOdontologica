f63ba34c28eb9beb0a966585d476c3fbe4afdf9a
Ettore Muniz <ettore.barbosa@gmail.com>
2022-11-28 09:59:21 -0300

    add: corsorigin nos controllers

--- src/main/java/com/dh/clinicaodonto/controller/ConsultaController.java
+++ src/main/java/com/dh/clinicaodonto/controller/ConsultaController.java
@@ -5,20 +5,13 @@ import com.dh.clinicaodonto.dto.ConsultaMarcacaoDto;
 import com.dh.clinicaodonto.service.impl.ConsultaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("consultas")
+@CrossOrigin
 public class ConsultaController {
 
    @Autowired
