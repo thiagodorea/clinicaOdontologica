87a8d1473fd72624a02eef69f672a1d28a2f9e6d
thiagodorea <thiago.dorea@hotmail.com>
2022-12-07 19:42:06 -0300

    remove importações não utilizadas

--- src/main/java/com/dh/clinicaodonto/dto/ConsultaMarcacaoDto.java
+++ src/main/java/com/dh/clinicaodonto/dto/ConsultaMarcacaoDto.java
@@ -9,7 +9,6 @@ import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
 import java.time.LocalDateTime;
 
 @Getter
